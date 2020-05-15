1ec89d3b295648d8206c8f16867522d8f571c399
Shellywell123 <benjamin@shellswell.com>
2020-05-15 23:38:29 +0100

    'exit' and invalid querys added

--- query.py
+++ query.py
@@ -4,13 +4,18 @@ from minor_chords import *
 
 def ask(maj_chords,min_chords):
     chord = raw_input('#######################\ninput a chord.\n')
-    
+
+    if chord == 'exit':
+        return 0    
 
     if chord in maj_chords:
         print eval(chord)
 
     if chord in min_chords:
         print eval(chord)
+
+    if (chord not in maj_chords) and (chord not in min_chords):
+        print '"'+chord+'" not in database'
 
     ask(maj_chords,min_chords)
 
